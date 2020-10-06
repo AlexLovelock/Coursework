ec8acf40ba922cd9eb8d0ccd406f2d603d1c697d
92956 <92956@farnborough.ac.uk>
2020-10-06 12:03:35 +0100

    6/10 push

---
+++ src/main/java/controllers/Teams.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("Teams/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Teams {
+    @GET
+    @Path("list")
+    public String TeamsList() {
+        System.out.println("Invoked Teams.TeamsList()");
+        JSONArray response;
+        response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT TeamID, TeamName FROM Teams");
+            ResultSet results = ps.executeQuery();
+            while (results.next() == true) {
+                JSONObject row = new JSONObject();
+                row.put("TeamID", results.getInt(1));
+                row.put("TeamName", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+}
+
